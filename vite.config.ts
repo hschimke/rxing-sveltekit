f25c9a6d83079f1ff07427e207d5c34684252c92
henry <henry@azule.info>
2023-03-07 16:47:45 +0000

    add compression and fixed build

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+import { compression } from 'vite-plugin-compression2'
 
 const config: UserConfig = {
 	plugins: [
 		sveltekit(),
 		wasm(),
-		topLevelAwait()
+		topLevelAwait(),
+		compression({
+			"algorithm": "brotliCompress",
+		})
 	]
 };
 
